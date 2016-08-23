37691c5eff230173beb582df92b22607c2b180ba
pradeep.cr <pradeep.cr@nestgroup.net>
2016-08-23 10:15:28 +0530

    Homework-1 (p1)

--- ShoppingCart3/api.js
+++ ShoppingCart3/api.js
@@ -101,3 +101,27 @@ module.exports= function(wagner) {
 	}));
 	return api;	
 }
+
+function handleOne(propery, res, error, result) {
+	if(error) {
+		return res.status(status.INTERNAL_SERVER_ERROR).json({error:error.toString()});	
+	}
+	if(!result) {
+		return res.status(status.NOT_FOUND).json({error:"Not found"});	
+	}
+	var json = {};
+	json[propery] = result;
+	res.json(json);
+}
+
+function handleMany(property, res, error, result) {
+  if (error) {
+    return res.
+      status(status.INTERNAL_SERVER_ERROR).
+      json({ error: error.toString() });
+  }
+
+  var json = {};
+  json[property] = result;
+  res.json(json);
+}
--- ShoppingCart3/index.js
+++ ShoppingCart3/index.js
@@ -4,7 +4,9 @@ var wagner = require('wagner-core');
 require('./models')(wagner);
 
 var app = express();
-//wagner.invoke(require('./auth'),{ app:app });
+//app.use(require('morgan')());
+
+wagner.invoke(require('./auth'), { app: app });
 app.use('/api/v1', require('./api')(wagner));
 
 app.listen(3000);
